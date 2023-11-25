fefd27573aae075cd606f0296be731a3ddfe75f2
georgia witchel <127355997+georgia-witchel@users.noreply.github.com>
2023-11-25 12:07:05 -0800

    protagonist can plant ghost

--- Assets/Planting.cs
+++ Assets/Planting.cs
@@ -8,14 +8,19 @@ public class Planting : MonoBehaviour
 {
     public Tilemap tilemap; // Assign this in the inspector or find it dynamically
     private GameObject protagonist;
+    private Rigidbody2D protagonistRB;
 
     private SpriteRenderer spriteRenderer;
 
     public GameObject BasicPlant; 
+    public GameObject GhostPlant; 
+
+    private bool GhostPlaced = false; 
     void Start()
     {
         // Find the Protagonist GameObject
         protagonist = GameObject.Find("Protagonist");
+        protagonistRB = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
 
 
@@ -37,9 +42,22 @@ public class Planting : MonoBehaviour
 
     void Update()
     {
-        if(canPlant() && Input.GetKeyDown(KeyCode.P))
+        // if the protagonist is moves destroy the ghost 
+        if (Mathf.Abs(protagonistRB.velocity.x) > 0.1f || Mathf.Abs(protagonistRB.velocity.x) > 0.1f)
         {
-            Plant();
+            GhostPlaced = false; 
+            GameObject ghostPlant = GameObject.Find("ghostPlant(Clone)");
+            Destroy(ghostPlant);
+        }
+       
+        if(canPlant() && !GhostPlaced && Input.GetKeyDown(KeyCode.P))
+        {
+            DisplayGhost(); 
+        } 
+        else if(canPlant() && GhostPlaced && Input.GetKeyDown(KeyCode.P))
+        {
+            ReplaceGhostPlantWithPlant();
+            
         }
         
     }
@@ -63,15 +81,46 @@ public class Planting : MonoBehaviour
 
     }
 
-    public void Plant(){
-        Debug.Log(protagonist.transform.position);
+    public void DisplayGhost(){
+        // calculate spawn position on tilemap
         Vector3Int protagonistTilePosition = tilemap.WorldToCell(protagonist.transform.position);
-        Debug.Log(protagonistTilePosition);
-        // Calculate the position of the tile to the left (-1 on the x-axis)
-        Vector3 spawnPosition = tilemap.CellToWorld(new Vector3Int(protagonistTilePosition.x - 1, protagonistTilePosition.y+1, protagonistTilePosition.z));
-        // Vector2 spawnPosition = spriteRenderer.flipX ? protagonist.transform.position + new Vector3(-1,0.3f,0) : protagonist.transform.position + new Vector3(1,0.3f,0);
-        // tilemap.CellToWorld(tilemapCoordinates);
-        Debug.Log(spawnPosition);
+        Vector3 spawnPosition = spriteRenderer.flipX ? tilemap.CellToWorld(new Vector3Int(protagonistTilePosition.x - 1, protagonistTilePosition.y, protagonistTilePosition.z)) : tilemap.CellToWorld(new Vector3Int(protagonistTilePosition.x + 1, protagonistTilePosition.y, protagonistTilePosition.z));
+
+        // spawn a plant at the target position
+        Instantiate(GhostPlant, spawnPosition, Quaternion.identity);
+        GhostPlaced = true;
+
+    }
+    public void Plant(){
+        // replaces the ghost plant with a real plant 
+        // calculate spawn position on tilemap
+        Vector3Int protagonistTilePosition = tilemap.WorldToCell(protagonist.transform.position);
+        Vector3 spawnPosition = spriteRenderer.flipX ? tilemap.CellToWorld(new Vector3Int(protagonistTilePosition.x - 1, protagonistTilePosition.y, protagonistTilePosition.z)) : tilemap.CellToWorld(new Vector3Int(protagonistTilePosition.x + 1, protagonistTilePosition.y, protagonistTilePosition.z));
+        Vector3Int blockBelowSpawnPosition = spriteRenderer.flipX ? new Vector3Int(protagonistTilePosition.x - 1, protagonistTilePosition.y-1, protagonistTilePosition.z) : new Vector3Int(protagonistTilePosition.x + 1, protagonistTilePosition.y-1, protagonistTilePosition.z);
+
+        // spawn a plant at the target position
         Instantiate(BasicPlant, spawnPosition, Quaternion.identity);
+
+        // clear the tile below the target plant so that the protagonist can't plant two plants at once 
+        tilemap.SetTile(blockBelowSpawnPosition, null);
+    }
+
+    public void ReplaceGhostPlantWithPlant()
+    {
+        GameObject ghostPlant = GameObject.Find("ghostPlant(Clone)");
+   
+        Vector3Int ghostPlantTilePosition = tilemap.WorldToCell(ghostPlant.transform.position);
+        Vector3Int blockBelowSpawnPosition =  new Vector3Int(ghostPlantTilePosition.x , ghostPlantTilePosition.y-1, ghostPlantTilePosition.z);
+
+
+        // Instantiate the Plant prefab at the position and rotation of ghostPlant
+        GameObject newPlant = Instantiate(BasicPlant, ghostPlant.transform.position, ghostPlant.transform.rotation);
+
+        // Optionally, set the new plant's parent to ghostPlant's parent
+        newPlant.transform.parent = ghostPlant.transform.parent;
+        tilemap.SetTile(blockBelowSpawnPosition, null);
+
+        // Destroy the ghostPlant
+        Destroy(ghostPlant);
     }
 }
