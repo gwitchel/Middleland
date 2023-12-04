8230f2e2a0fd7b5133c6e345e2327de770f705b3
georgia witchel <127355997+georgia-witchel@users.noreply.github.com>
2023-12-03 21:16:23 -0800

    changed layer interactions

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab; // Set this to your enemy prefab in the inspector
     public float averageRadius = 2f; // Average radius from the GameObject
     public float radiusVariance = 1f; // Variance in the radius
+    public Tilemap tilemap;
     void Start()
     {
-        
+        GameObject gridGameObject = GameObject.Find("Grid");
+        tilemap = gridGameObject.transform.Find("test_map_contact").GetComponent<Tilemap>();        
     }
 
     // Update is called once per frame
@@ -41,7 +44,7 @@ public class EnemySpawner : MonoBehaviour
 
             // Calculate the spawn position
             // Vector2 potentialSpawnPosition = new Vector2(transform.position.x,transform.position.y) + randomRadius * new Vector2(Random.value < 0.5f ? -1 : 1, 0);
-            Vector2 potentialSpawnPosition = new Vector2(transform.position.x+0.75f,transform.position.y+0.75f); // + randomRadius * new Vector2(Random.value < 0.5f ? -1 : 1, 0);
+            Vector2 potentialSpawnPosition = new Vector2(transform.position.x,transform.position.y) + randomRadius * new Vector2(Random.value < 0.5f ? -1 : 1, 0.75f);
 
             // Check if the position is valid (not overlapping and on the same surface)
             if(IsValidSpawnPosition(potentialSpawnPosition))
@@ -55,14 +58,21 @@ public class EnemySpawner : MonoBehaviour
     bool IsValidSpawnPosition(Vector2 position)
     {
         // Check for overlap with other colliders
-        if(Physics2D.OverlapPoint(position)) // Adjust the radius as needed for your game
-        {
-            return false;
-        }
+        // if(Physics2D.OverlapPoint(position)) // Adjust the radius as needed for your game
+        // {
+        //     return false;
+        // }
+
+        return(IsPositionFilled(position));
 
         // Optionally, add additional checks here, like ensuring the enemy is on the same surface
 
-        return true;
+        // return true;
+    }
+    public bool IsPositionFilled(Vector2 worldPosition)
+    {
+        Vector3Int cellPosition = tilemap.WorldToCell(new Vector3(worldPosition.x,worldPosition.y,0)); // Convert world position to cell position
+        return tilemap.GetTile(cellPosition) != null; // Check if there is a tile at this position
     }
     
         
